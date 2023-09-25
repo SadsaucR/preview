73fa8ca99edbfcdc9f9e6af6c36b4de74c36b347
SadsaucR <cks6023159@gmail.com>
2023-09-26 03:56:11 +0800

    update

---
+++ L1.c
@@ -0,0 +1,5 @@
+#include<stdio.h> /*printf函數位於此標頭檔*/
+void L1() /*字串輸出練習*/
+{
+	printf("Hello world!"); 
+}
---
+++ L1_2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void L1_2()
+{
+	int a = 2; /*宣告一個整數，值為2*/
+	printf("我有%d隻手", a);
+}
